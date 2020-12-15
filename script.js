ab148a7f2a32069ae97694c10c933af54f4e8626
WazeAzure <bryan.rich0604@gmail.com>
2020-12-15 11:51:22 +0800

    addin event click

--- script.js
+++ script.js
@@ -2,9 +2,3 @@
 alert("woking!")
 
 //move gunction
-function move(){
-	liff.openWindow({
-            url: 'https://21cineplex.com', // Isi dengan Endpoint URL aplikasi web Anda
-            external: true
-        });
-}
