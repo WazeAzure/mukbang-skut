94b0b19b18c49a585c0a9e54baa12679d98b0db3
WazeAzure <bryan.rich0604@gmail.com>
2020-12-15 13:18:05 +0800

    updating features

--- liff-starter.js
+++ liff-starter.js
@@ -1,37 +1,89 @@
-window.onload = function() {
-    const useNodeJS = false;
-    const defaultLiffId = process.env.MY_LIFF_ID;
- 
-    // DO NOT CHANGE THIS
-    let myLiffId = "";
-};
+window.onload = function(){
+	const defaultLiffId = "1655378358-J317E6Xo";
+	initializeLiffOrDie(defaultLiffId);
+}
 
-function move(){
-	liff.openWindow({
-            url: 'https://mukbang-skut.herokuapp.com/',
+function initializeLiffOrDie(myLiffId){
+	initializeLiff(myLiffId);
+}
+
+function initializeLiff(myLiffId){
+	liff
+		.init({
+			liffid: myLiffId
+		})
+		.then(() => {
+			initializeApp();
+		})
+}
+
+function initializeApp(){
+	displayLiffData();
+    displayIsInClientInfo();
+    registerButtonHandlers();
+	
+	if (liff.isLoggedIn()) {
+        document.getElementById('logInButton').disabled = true;
+    } else {
+        document.getElementById('liffLogoutButton').disabled = true;
+    }
+}
+
+function displayLiffData() {
+    document.getElementById('isInClient').textContent = liff.isInClient();
+    document.getElementById('isLoggedIn').textContent = liff.isLoggedIn();
+}
+
+function displayIsInClientInfo() {
+    if (liff.isInClient()) {
+        document.getElementById('logInButton').classList.toggle('hidden');
+        document.getElementById('logOutButton').classList.toggle('hidden');
+        document.getElementById('isInClientMessage').textContent = 'You are opening the app in the in-app browser of LINE.';
+    } else {
+        document.getElementById('isInClientMessage').textContent = 'You are opening the app in an external browser.';
+    }
+}
+function registerButtonHandlers() {
+    document.getElementById('extendwButton').addEventListener('click', function() {
+        liff.openWindow({
+            url: 'https://mukbang-skut.herokuapp.com/', // Isi dengan Endpoint URL aplikasi web Anda
             external: true
         });
-}
-
-function loginA(){
-	if(!liff.isLogIn){
-		liff.login();
-	}
-}
-
-function closeA(){
-	liff.closeWindow();
-}
-
-function sendA(){
-	liff.sendMessage([{
-		type:"text",
-		text:"Halo, DIcoding!"
-	}])
-	.then(() => {
-		console.log('message sent');
-	})
-	.catch((err) => {
-		console.log('error', err);
-	});
+    });
+ 
+ document.getElementById('closeButton').addEventListener('click', function() {
+        if (!liff.isInClient()) {
+            sendAlertIfNotInClient();
+        } else {
+            liff.closeWindow();
+        }
+    });
+     document.getElementById('logInButton').addEventListener('click', function() {
+        if (!liff.isLoggedIn()) {
+            liff.login();
+        }
+    });
+ 
+ 
+    document.getElementById('logOutButton').addEventListener('click', function() {
+        if (liff.isLoggedIn()) {
+            liff.logout();
+            window.location.reload();
+        }
+    });
+ 
+    document.getElementById('sendButton').addEventListener('click', function() {
+        if (!liff.isInClient()) {
+            sendAlertIfNotInClient();
+        } else {
+            liff.sendMessages([{
+                'type': 'text',
+                'text': "Anda telah menggunakan fitur Send Message!"
+            }]).then(function() {
+                window.alert('Ini adalah pesan dari fitur Send Message');
+            }).catch(function(error) {
+                window.alert('Error sending message: ' + error);
+            });
+        }
+    });
 }
