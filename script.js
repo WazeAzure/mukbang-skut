3071bf364669acf0bd68d0891fe5aa4c16198633
WazeAzure <bryan.rich0604@gmail.com>
2020-12-20 15:24:34 +0800

    Fixing Enter

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function count(){
 			hargaView.innerHTML = `${input} ${listmakanan[i].nama}\t\t: ${total}`;
 			var element = document.getElementById("kesimpulan-makanan");
 			element.append(hargaView);
-			totPesanan.push(`${input} ${listmakanan[i].nama}\t\t: ${total}\n`);
+			totPesanan.push(`\n${input} ${listmakanan[i].nama}: ${total}`);
 		};
 	}
 	console.log("---------------------");
